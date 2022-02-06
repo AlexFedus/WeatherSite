c0e0907edbf28d2f7d86ae79b2c92d7bc20ec6d4
AlexFedus <83235408+AlexFedus@users.noreply.github.com>
2022-02-06 17:29:14 -0500

    Began api fetching

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+let weather = {
+   "apiKey": "69087aca4a5504e05b12280a1a13a322",
+   fetchWeather: function () {
+       fetch( 
+           "https://api.openweathermap.org/data/2.5/weather?q={Denver}&appid={69087aca4a5504e05b12280a1a13a322}"
+       ).then((response) =>  response.json())
+       .then((data) => console.log(data));
+   },
+};
