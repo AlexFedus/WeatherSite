e2c1f4b9ad6cd3b8b2e6405e4d3c67e33b92d8fa
AlexFedus <83235408+AlexFedus@users.noreply.github.com>
2022-02-08 12:06:35 -0500

    Update script.js
    converted degrees to fahrenheit

--- script.js
+++ script.js
@@ -10,10 +10,13 @@ let weather = {
    displayWeather: function(data) {
         const { name } = data;
         const { icon, description } = data.weather[0];
-        const { temp, humidity } = data.main;
+        var { temp, humidity } = data.main;
         const { speed } = data.wind;
         
-        console.log(name, icon, description, temp, humidity, speed); 
+        
+        temp= ((temp -273.15)*1.8)+32;
+        temp = temp.toFixed(1);
+
         document.querySelector(".city").innerText = "Weather in " + name;
         document.querySelector(".icon").src = "https://openweathermap.org/img/wn/"+ icon + "@2x.png";
         document.querySelector(".description").innerText = description;
