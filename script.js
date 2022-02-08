adb05e4b98ab31480b3dc13a069c913965527edc
AlexFedus <83235408+AlexFedus@users.noreply.github.com>
2022-02-08 15:14:21 -0500

    Geolocation
    added ability to get weather based on geolocation

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
+getLocation()
+
+function getLocation(){
+    navigator.geolocation.getCurrentPosition((success) => {
+        console.log(success);
+
+        let {latitude, longitude} = success.coords;
+
+        fetch( 
+            "https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&units=imperial&appid="+ weather.apiKey
+        ).then((response) =>  response.json())
+        .then((data) => weather.displayWeather(data));
+    
+
+    })
+}
+    
 let weather = {
    "apiKey": "69087aca4a5504e05b12280a1a13a322",
+
+
+    
    fetchWeather: function (city) {
+       
        fetch( 
-           "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid="+ this.apiKey
+           "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid="+ this.apiKey
        ).then((response) =>  response.json())
        .then((data) => this.displayWeather(data));
    },
@@ -11,19 +32,25 @@ let weather = {
         const { name } = data;
         const { icon, description } = data.weather[0];
         var { temp, humidity } = data.main;
-        const { speed } = data.wind;
+        var { speed } = data.wind;
         
         
-        temp= ((temp -273.15)*1.8)+32;
         temp = temp.toFixed(1);
 
+      
+       
+
+        
         document.querySelector(".city").innerText = "Weather in " + name;
         document.querySelector(".icon").src = "https://openweathermap.org/img/wn/"+ icon + "@2x.png";
         document.querySelector(".description").innerText = description;
-        document.querySelector(".temperature").innerText = temp;
-        document.querySelector(".humidity").innerText = humidity;
-        document.querySelector(".speed").innerText = speed;
+        document.querySelector(".temperature").innerText = temp + " \u2109";
+        document.querySelector(".humidity").innerText = "Humididy: " + humidity + "%";
+        document.querySelector(".speed").innerText = "Wind: " + speed + " MPH";
    },
+
+
+
 
    search: function() {
        this.fetchWeather(document.querySelector(".searchBar").value);
@@ -41,5 +68,5 @@ document.querySelector(".button").addEventListener("keyup", function (event) {
         weather.search();
     }
     
-
+    
 })
