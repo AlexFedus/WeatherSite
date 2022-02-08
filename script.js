58fbcbb88db7db99229484ff00b9c884634534bc
AlexFedus <83235408+AlexFedus@users.noreply.github.com>
2022-02-08 10:47:22 -0500

    api support
    Added ability to search for a city and return current weather of said city. Formatting still not correct

--- script.js
+++ script.js
@@ -1,9 +1,42 @@
 let weather = {
    "apiKey": "69087aca4a5504e05b12280a1a13a322",
-   fetchWeather: function () {
+   fetchWeather: function (city) {
        fetch( 
-           "https://api.openweathermap.org/data/2.5/weather?q={Denver}&appid={69087aca4a5504e05b12280a1a13a322}"
+           "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid="+ this.apiKey
        ).then((response) =>  response.json())
-       .then((data) => console.log(data));
+       .then((data) => this.displayWeather(data));
    },
+
+   displayWeather: function(data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        
+        console.log(name, icon, description, temp, humidity, speed); 
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/"+ icon + "@2x.png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temperature").innerText = temp;
+        document.querySelector(".humidity").innerText = humidity;
+        document.querySelector(".speed").innerText = speed;
+   },
+
+   search: function() {
+       this.fetchWeather(document.querySelector(".searchBar").value);
+   }
+
 };
+
+document.querySelector(".button").addEventListener("click", function () {
+    weather.search();
+
+})
+
+document.querySelector(".button").addEventListener("keyup", function (event) {
+    if (event.key == "Enter" || event.keyCode == 13) {
+        weather.search();
+    }
+    
+
+})
