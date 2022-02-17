8e434d0ed172edb0733bb7ea289efba2b402c5ef
AlexFedus <83235408+AlexFedus@users.noreply.github.com>
2022-02-16 19:06:02 -0500

    Fixed indentation and added js comments

--- script.js
+++ script.js
@@ -1,38 +1,39 @@
 var id = "";
 
 let weather = {
-   "apiKey": "69087aca4a5504e05b12280a1a13a322",
+    "apiKey": "69087aca4a5504e05b12280a1a13a322",
 
-
-   getLocation: function(){
+    // Function to get location if "use my location" button is clicked
+    getLocation: function () {
         navigator.geolocation.getCurrentPosition((success) => {
-        document.getElementById("box2").style.visibility = "hidden";
-        console.log(success);
+            document.getElementById("box2").style.visibility = "hidden";
+            console.log(success);
 
-        let {latitude, longitude} = success.coords;
+            let { latitude, longitude } = success.coords;
 
-        fetch( 
-            "https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&units=imperial&appid="+ weather.apiKey
-        ).then((response) =>  response.json())
-        .then((data) => weather.displayWeather(data));
-    })
-   },
-    
-   fetchWeather: function (city) {
-       
-       fetch( 
-           "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid="+ this.apiKey
-       ).then((response) =>  response.json())
-       .then((data) => 
-       //this.getID(data),
-       this.displayWeather(data));
-       
-   },
+            fetch(
+                "https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&units=imperial&appid=" + weather.apiKey
+            ).then((response) => response.json())
+                .then((data) => weather.displayWeather(data));
+        })
+    },
 
-  
+    // Function to get weather information from api
+    fetchWeather: function (city) {
 
-   displayWeather: function(data) {
-    
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + this.apiKey
+        ).then((response) => response.json())
+            .then((data) =>
+                this.displayWeather(data));
+
+    },
+
+
+
+    // Function to display weather information
+    displayWeather: function (data) {
+
 
         id = data.id;
         console.log(id);
@@ -40,37 +41,39 @@ let weather = {
         const { icon, description } = data.weather[0];
         var { temp, humidity } = data.main;
         var { speed } = data.wind;
-        
-        
+
+
         temp = temp.toFixed(1);
 
-      
-       
 
-        
+
+
+
         document.querySelector(".city").innerText = "Weather in " + name;
-        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/"+ icon + "@2x.png";
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
         document.querySelector(".description").innerText = description;
         document.querySelector(".temperature").innerText = temp + " \u2109";
         document.querySelector(".humidity").innerText = "Humididy: " + humidity + "%";
         document.querySelector(".speed").innerText = "Wind: " + speed + " MPH";
 
-        document.getElementById("forecast").style.visibility = "visible"; 
+        document.getElementById("forecast").style.visibility = "visible";
 
-        
-   },
 
-   fetchForcast: function (id){
-        fetch( 
-            "https://api.openweathermap.org/data/2.5/forecast?id=" + id + "&units=imperial&appid="+ this.apiKey
-        ).then((response2) =>  response2.json())
-        .then((data2) => 
-        weather.displayForcast(data2));
-        
-   },
+    },
 
-   displayForcast: function(data2) {
-        
+    // Function to get the forecast from the api.
+    fetchForcast: function (id) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/forecast?id=" + id + "&units=imperial&appid=" + this.apiKey
+        ).then((response2) => response2.json())
+            .then((data2) =>
+                weather.displayForcast(data2));
+
+    },
+
+    // Function to display the forecast
+    displayForcast: function (data2) {
+
         //temperatures
         document.querySelector(".temperature1").innerText = Number(data2.list[0].main.temp).toFixed(1) + " \u2109";
         document.querySelector(".temperature2").innerText = Number(data2.list[1].main.temp).toFixed(1) + " \u2109";
@@ -95,54 +98,61 @@ let weather = {
         document.querySelector(".speed2").innerText = "Wind: " + Number(data2.list[1].wind.speed).toFixed(1) + " MPH";
         document.querySelector(".speed3").innerText = "Wind: " + Number(data2.list[2].wind.speed).toFixed(1) + " MPH";
 
-       
-        
-   },
+
+
+    },
 
 
 
 
-   search: function() {
-       document.getElementById("box2").style.visibility = "hidden";
-       this.fetchWeather(document.querySelector(".searchBar").value);
-   },
+    // Function to search for weather with search bar input
+    search: function () {
+        document.getElementById("box2").style.visibility = "hidden";
+        this.fetchWeather(document.querySelector(".searchBar").value);
+    },
 
-   search2: function() {
+    // Function to get weather by geolocation
+    search2: function () {
         document.getElementById("box2").style.visibility = "hidden";
         this.getLocation();
-   },
+    },
 
 
-   showForcast: function(){
+    //Function that alters the visibility of the forecast box
+    showForcast: function () {
         document.getElementById("box2").style.visibility = "visible";
 
-   }
+    }
 
 
 
 };
 
+// Searches for weather when search button is clicked
 document.querySelector(".button").addEventListener("click", function () {
     weather.search();
 
 })
 
+// Gets the users geolocation when button is clicked
 document.querySelector(".location").addEventListener("click", function () {
     weather.getLocation();
 
 })
 
+//Shows weather forecast when button is clicked
 document.querySelector("#forecast").addEventListener("click", function () {
     weather.showForcast();
     weather.fetchForcast(id);
-    
+
 
 })
 
+//
 document.querySelector(".button").addEventListener("keyup", function (event) {
     if (event.key == "Enter" || event.keyCode == 13) {
         weather.search();
     }
-    
-    
+
+
 })
